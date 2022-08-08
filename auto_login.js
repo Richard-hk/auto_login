b890114d7e22427d63b0fb743f3c84da32abdd96
Richard-hk <30943685+Richard-hk@users.noreply.github.com>
2022-08-08 16:06:44 +0800

    init

---
+++ auto_login.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         网站自动登录脚本
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  auto login all the website
+// @author       Richard-hk
+// @license      MIT
+// @match        https://www.tampermonkey.net/scripts.php
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
